f38878069013a19810311d95765985d9da5e77b2
syednaseer533.sn@gmail.com <syednaseer533.sn@gmail.com>
2022-12-20 15:13:49 +0530

    Exercise 2-7A Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,18 +1,8 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import getStudents from '@salesforce/apex/studentBrowser.getStudents';
 
 export default class StudentBrowser extends LightningElement {
-	studentList = [];
-	constructor() {
-		// Call the super constructor here...
-		super();
-		const studentNames = ["Rad", "Stuart", "Andres", "Rahul", "Amit", "Simon"];
-		this.studentList = studentNames.map((studentName, index) => {
-			return {
-				sobjectType: "Contact",
-				Name: studentName,
-				PhotoUrl: "/services/images/photo/003B0FakePictId",
-				Id: index
-			};
-		});
-	}
-}
+    @wire(getStudents, {intructorId: "", courseDeliveryId: ""})	
+    students;
+};
+		
