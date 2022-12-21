873e8672cf970ddc02f96402bf1e358617e6731d
syednaseer533.sn@gmail.com <syednaseer533.sn@gmail.com>
2022-12-21 22:58:45 +0530

    Exercise 2-9 Contacts queried

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,8 +1,16 @@
-import { LightningElement, wire } from "lwc";
-import getStudents from '@salesforce/apex/studentBrowser.getStudents';
+import { LightningElement, wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 
 export default class StudentBrowser extends LightningElement {
-    @wire(getStudents, {intructorId: "", courseDeliveryId: ""})	
-    students;
-};
-		
+	@wire(getStudents, { instructorId: '$selectedInstructorId', courseDeliveryId: '$selectedDeliveryId' })
+	students;
+
+	selectedDeliveryId = '';
+	selectedInstructorId = '';
+
+	handleFilterChange(event){
+		this.selectedDeliveryId = event.detail.deliveryId;
+		this.selectedInstructorId = event.detail.instructorId;
+	}
+
+}
