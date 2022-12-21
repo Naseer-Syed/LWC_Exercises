180b5f9109144d8134dd66651d7ea76c0a255209
syednaseer533.sn@gmail.com <syednaseer533.sn@gmail.com>
2022-12-21 23:20:23 +0530

    Exercise 2-10 Completed

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -1,48 +1,56 @@
 import { LightningElement, wire } from 'lwc';
 
-// TODO #1: import the getRecord, getFieldValue, and getFieldDisplayValue functions from lightning/uiRecordApi.
-
-// TODO #2: We've imported the name field and placed it into an array for you.
-//          To prepare for Lab 1, import the Description, Email, and Phone fields and add them to the array.
 import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
+import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
+import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
+
+
 import FIELD_Name from '@salesforce/schema/Contact.Name';
 import FIELD_Description from '@salesforce/schema/Contact.Description';
 import FIELD_Email from '@salesforce/schema/Contact.Email';
 import FIELD_Phone from '@salesforce/schema/Contact.Phone';
-
-const fields = [FIELD_Name, FIELD_Description, FIELD_Email,
-    FIELD_Phone];
+const fields = [FIELD_Name, FIELD_Description, FIELD_Email, FIELD_Phone];
 
 export default class StudentDetail extends LightningElement {
 
-	// TODO #3: locate a valid Contact ID in your scratch org and store it in the studentId property.
-	// Example: studentId = '003S000001SBAXEIA5';
-    studentId = '003DP00001Q0DrbYAF';
+	studentId;
+	subscription;
 
-	//TODO #4: use wire service to call getRecord, passing in our studentId and array of fields.
-	//		   Store the result in a property named wiredStudent.
-    @wire(getRecord, { recordId: '$studentId', fields })
-wiredStudent;
-		
+	@wire(MessageContext) messageContext;
+	
+	@wire(getRecord, { recordId: '$studentId', fields })
+	wiredStudent;
+	
+	connectedCallback() {
+		if(this.subscription){
+			return;
+		}
+		this.subscription = subscribe(
+			this.messageContext, 
+			SELECTED_STUDENT_CHANNEL,
+			(message) => {
+				this.handleStudentChange(message)
+			}
+		);
+	}
+
+	disconnectedCallback() {
+		unsubscribe(this.subscription);
+		this.subscription = null;
+	}
+
 	get name() {
 		return this._getDisplayValue(this.wiredStudent.data, FIELD_Name);
 	}
-    get description() {
-        return this._getDisplayValue(this.wiredStudent.data,
-        FIELD_Description);
-        }
-        get phone() {
-        return this._getDisplayValue(this.wiredStudent.data,
-        FIELD_Phone);
-        }
-        get email() {
-            return this._getDisplayValue(this.wiredStudent.data,
-                FIELD_Email);
-                }
-	//TODO #5: We provided a getter for the name field. 
-	// 		   To prepare for Lab 1, create getters for the description, phone, and email fields.
-	
-	//TODO #6: Review the cardTitle getter, and the _getDisplayValue function below.
+	get description() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Description);
+	}
+	get phone() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Phone);
+	}
+	get email() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Email);
+	}
 	
 	get cardTitle() {
 		let title = "Please select a student";
@@ -52,6 +60,10 @@ wiredStudent;
 			title = "Something went wrong..."
 		}
 		return title;
+	}
+
+	handleStudentChange(message) {
+		this.studentId = message.studentId;
 	}
 	
 	_getDisplayValue(data, field) {
